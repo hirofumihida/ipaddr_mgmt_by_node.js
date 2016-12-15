bfbe5da69c8384a3c654ab14b58f51ae3ecd3f68
Hirofumi Hida <Hirofumi@Hirofumis-MacBook-Air.local>
2016-12-15 20:11:34 +0900

    add simple POST func

--- server/server.js
+++ server/server.js
@@ -9,6 +9,29 @@ app.get('/list_ipaddr', function (req, res) {
    });
 })
 
+var ipaddr = {
+        "IP004": {
+	    "INUSE":"yes",
+            "IPADDR":"192.168.0.4",
+            "OWNER":"hirofumi",
+            "RESERVED":"no",
+            "DHCP_RESERVED":"no",
+            "HOSTNAME":"test01",
+            "START":"2016-12-15",
+            "EXPIRE":"0000-00-00"
+       }
+}
+
+app.post('/req_ipaddr', function (req, res) {
+	// First read existing users.
+	fs.readFile( __dirname + "/" + "ipaddr.json", 'utf8', function (err, data) {
+	data = JSON.parse( data );
+	data["IP004"] = ipaddr["IP004"];
+	console.log( data );
+	res.end( JSON.stringify(data));
+	});
+})
+
 var server = app.listen(8081, function () {
 
   var host = server.address().address
